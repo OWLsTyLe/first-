0f9c9c3908da7fb605fe9678631c70a9dbcab2d2
OWLsTyLe <maxshypilkin@gmail.com>
2024-03-13 19:01:10 +0200

    Адмінка

--- my/main/models.py
+++ my/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name_you = models.CharField('Ваше імʼя', max_length=100)
+    name = models.CharField('Назва обʼяви', max_length=100)
+    pud_text = models.TextField('Введіть текст', max_length=10000)
+    pub_date = models.DateTimeField('date published')
+    def  __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Додати'
+        verbose_name_plural = 'Додати'
