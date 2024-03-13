4fd6992943fcb71bc4c5a6e54f9f53bc3b04b558
OWLsTyLe <maxshypilkin@gmail.com>
2024-03-13 19:02:58 +0200

    add urls and more

--- my/main/views.py
+++ my/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Category
 
 def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
 
