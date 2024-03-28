e71ec93a4bfe69950244b58daf230ead67f6643f
OWLsTyLe <maxshypilkin@gmail.com>
2024-03-28 19:04:54 +0200

    new views

--- my/main/views.py
+++ my/main/views.py
@@ -10,5 +10,9 @@ def about(request):
 def contact(request):
     ads = Category.objects.all()
     return render(request, 'main/contact.html', {'ads': ads})
-
-
+def js(request):
+    return render(request, 'main/js.html')
+def news(request):
+    return render(request, 'main/news.html')
+def stat(request):
+    return render(request, 'main/stat.html')
